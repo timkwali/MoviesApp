61f237c5701d15e83d4b52aa7602eb14671f62ba
timkwali <timkwalit@accessbankplc.com>
2022-09-29 12:41:59 +0100

    Initial commit

---
+++ movieslist/src/main/java/com/example/movieslist/presentation/MoviesListFragment.kt
@@ -0,0 +1,34 @@
+package com.example.movieslist.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieslist.R
+import com.example.movieslist.databinding.FragmentMoviesListBinding
+
+
+class MoviesListFragment : Fragment() {
+
+    private var _binding: FragmentMoviesListBinding? = null
+    private val binding: FragmentMoviesListBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMoviesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
